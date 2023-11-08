f0ac970d710448dab8a054fd8c0c54e121e13c1e
Zig <zvszig@gmail.com>
2023-11-08 15:30:24 -0800

    added 404 page

---
+++ index.js
@@ -0,0 +1,13 @@
+require('donenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello world!');
+});
+
+app.get('*', function(req, res){
+    res.status(404).send('<h1>404 Page</h1>');
+})
+
+app.listen(process.env.PORT);
